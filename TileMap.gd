0ed0682e794f5104f6e53d92c84c6ef89d421fc0
gdgsbnz185476 <68424966+gdgsbnz185476@users.noreply.github.com>
2024-06-20 23:00:07 +1200

    same chane to the platform for the death

--- TileMap.gd
+++ TileMap.gd
@@ -9,3 +9,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+# Called when the node enters the scene tree for the first time.
+
+# Function to handle when a body enters the spike area
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		body.kill() # Assuming the player has a 'kill' method to handle death
