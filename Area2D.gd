74b7893cb90b4b2330feb68e6efc3d64943cf3fe
gdgsbnz185476 <68424966+gdgsbnz185476@users.noreply.github.com>
2024-06-21 09:40:10 +1200

    level manager change

--- Area2D.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-# Called when the node enters the scene tree for the first time.
-
-# Function to handle when a body enters the spike area
-func _on_body_entered(body):
-	if body.is_in_group("player"):
-		body.kill() # Assuming the player has a 'kill' method to handle death
